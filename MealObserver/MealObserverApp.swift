aa9e34c9bffb004e0666be5a3f5f1b181bd804a7
Ivan Dekhtiarov <ivan.dekhtiarov@gmail.com>
2024-09-15 10:54:14 -0700

    Initial Commit

---
+++ MealObserver/MealObserverApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealObserverApp.swift
+//  MealObserver
+//
+//  Created by divan on 9/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MealObserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
