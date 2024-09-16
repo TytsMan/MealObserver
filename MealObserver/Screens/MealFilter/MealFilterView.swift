b8141c04d7ecde57b3b54ca68ce2b3f1496dfc1d
Ivan Dekhtiarov <ivan.dekhtiarov@gmail.com>
2024-09-15 20:32:17 -0700

    feat: Add Meal Filter.

--- MealObserver/Screens/MealFilter/MealFilterView.swift
+++ MealObserver/Screens/MealFilter/MealFilterView.swift
@@ -5,3 +5,65 @@
 //  Created by divan on 9/15/24.
 //
 
+import SwiftUI
+import Networking
+
+
+struct MealFilterView: View {
+    @State private var viewModel: ViewModel
+    @State private var searchText = ""
+    
+    init(viewModel: ViewModel, searchText: String = "") {
+        self.viewModel = viewModel
+        self.searchText = searchText
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                switch viewModel.listState {
+                case .default:
+                    VStack {
+                        Image(systemName: "swirl.circle.righthalf.filled")
+                            .frame(width: 36, height: 36)
+                            .foregroundColor(.gray)
+                        Text("Now you can start searching category")
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                    }
+                case .loading:
+                    ProgressView()
+                case .items(let meals):
+                    List {
+                        ForEach(meals, id: \.self) { meal in
+                            NavigationLink {
+                                Text(meal.title)
+                            } label: {
+                                MealView(meal: meal)
+                            }
+                        }
+                    }
+                case .error(let message):
+                    VStack {
+                        Image(systemName: "exclamationmark.triangle.fill")
+                            .frame(width: 36, height: 36)
+                            .foregroundColor(.red)
+                        Text(message)
+                            .font(.footnote)
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .navigationTitle("My Title")
+            
+        }
+        .searchable(text: $searchText, prompt: "Filter meal by category")
+        .onChange(of: searchText) { _, newValue in
+            viewModel.searchTextDidChanged(searchText: searchText)
+        }
+    }
+}
+
+#Preview {
+    MealFilterView(viewModel: .init())
+}
--- MealObserver/Screens/MealFilter/MealFilterViewModel.swift
+++ MealObserver/Screens/MealFilter/MealFilterViewModel.swift
@@ -5,3 +5,64 @@
 //  Created by divan on 9/15/24.
 //
 
+import Foundation
+import Combine
+import Networking
+
+extension MealFilterView {
+    
+    @Observable
+    final class ViewModel {
+        
+        enum ListState {
+            case `default`
+            case loading
+            case items([Meal])
+            case error(message: String)
+        }
+        
+        private var mealFilterService: MealFilterServiceProtocol
+        private var searchTextInputSubject = PassthroughSubject<String?, Never>()
+        private var cancellables = Set<AnyCancellable>()
+        
+        var listState: ListState = .default
+        
+        init(
+            mealFilterService: MealFilterServiceProtocol = MealFilterServiceSuccessMock()
+        ) {
+            self.mealFilterService = mealFilterService
+            self.searchTextInputSubject
+                .debounce(for: 0.3, scheduler: RunLoop.main)
+                .sink { [weak self] searchInput in
+                    Task { [weak self] in
+                        await self?.fetchItems(with: searchInput)
+                    }
+                }
+                .store(in: &cancellables)
+        }
+        
+        func searchTextDidChanged(searchText: String) {
+            if searchText.isEmpty {
+                Task {
+                    await fetchItems(with: nil)
+                }
+            } else {
+                searchTextInputSubject.send(searchText)
+            }
+        }
+        
+        private func fetchItems(with searchText: String?) async {
+            guard let searchText, !searchText.isEmpty else {
+                listState = .default
+                return
+            }
+            let result = await mealFilterService.filterMeals(query: searchText, filterType: .category)
+            switch result {
+            case .success(let responce):
+                listState = .items(responce.meals)
+            case .failure(let error):
+                listState = .error(message: error.description)
+            }
+        }
+    }
+}
